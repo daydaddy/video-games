436b318a3f57d9da3f0622f7a8ca580187074554
daydaddy <jn_day@hotmail.com>
2020-09-22 14:58:06 -0600

    added frontend for read

---
+++ presentation/src/components/VideoGames.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import VideoGame from './VideoGame';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export default class extends Component {
+    state = {
+        videoGames: []
+    }
+    getVideoGames = () => {
+
+        fetch(`${API_URL}/video-games`)
+            .then(response => response.json())
+            .then(videoGames => this.setState({ videoGames }))
+    }
+    componentDidMount() {
+        this.getVideoGames();
+    }
+    render() {
+        const displayGames = this.state.videoGames
+        .map(game => <VideoGame game={game}/>)
+        return (
+            <div>
+                <h1>Video Games</h1>
+            {displayGames}
+            </div>
+        )
+    }
+}
