88e82d566e2c8ffedf65c402b26bdd83d7e84bd7
daydaddy <jn_day@hotmail.com>
2020-09-23 11:02:48 -0600

    added POST API

--- routes/api/video-games.js
+++ routes/api/video-games.js
@@ -4,18 +4,31 @@ const router = express.Router();
 const db = require('../../data/mongo');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const info = {
-      quert: {},
-      collection: req.app.locals.collectionVideoGames
-  }
-  db.readAll(info)
-    .then((videoGames) => {
-        res.json(videoGames);
-    })
-    .catch(err => {
-        console.log(err);
-    })
+router.get('/', function (req, res, next) {
+    const info = {
+        query: {},
+        collection: req.app.locals.collectionVideoGames
+    }
+    db.readAll(info)
+        .then((videoGames) => {
+            res.json(videoGames);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 });
 
+router.post('/', function (req, res, next) {
+    const info = {
+        doc: req.body,
+        collection: req.app.locals.collectionVideoGames
+    }
+    db.createOne(info)
+        .then((data) => {
+            res.json(data.ops [0]);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
 module.exports = router;
