f776c7b60855fbdf5427ac6ed02461fbdbce4daa
daydaddy <jn_day@hotmail.com>
2020-09-25 11:08:16 -0600

    Deployment 01

---
+++ public/precache-manifest.c927608db6a358d6e264257bedece7d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f727685697a5048dd1fa7d432df25019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d8a8ccc4061b2253d78",
+    "url": "/static/css/main.fdf329e9.chunk.css"
+  },
+  {
+    "revision": "126b3307539c1736acb9",
+    "url": "/static/js/2.9e65a48f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e65a48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d8a8ccc4061b2253d78",
+    "url": "/static/js/main.53234f74.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  }
+]);
